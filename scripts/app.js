a300c042bcf859f4ff4e935216a2e939fe110ff4
TimLeary <dnasztanovics@gmail.com>
2016-02-19 12:20:52 +0100

    working alpha

--- scripts/app.js
+++ scripts/app.js
@@ -2,20 +2,43 @@ var laszlovector = angular.module('laszlovector', [])
 	.controller('referenceList', function ($scope) {
 		$scope.init = function(){
 			$scope.references = laszlovectorElements;
-			$scope.setIsotope();
 		};
 
-		$scope.setIsotope = function(){
-			var iso = new Isotope('.referenceList',{
-				masonry: {
-					columnWidth : 160
-				},
-				itemSelector:'.referenceItems',
-				layoutMode:'masonry',
-				resizesContainer:true,
-				animationEngine:'jQuery'
-			});
-		};
+		$scope.lightboxHide = function(index){
+			if(index != 0){
+				return 'hide';
+			} else {
+				return '';
+			}
+		}
 
 		$scope.init();
+	});
+
+
+	laszlovector.directive('afterRender', function () {
+		return {
+		  link: function (scope, element, attrs) {
+		  	console.log('scope - ',scope.reference);
+			if (scope.$last){
+				window.isotopeTimer = setInterval(function(){ 
+					var iso = new Isotope('.referenceList',{
+						masonry: {
+							columnWidth : 160
+						},
+						//itemSelector:'.referenceItems',
+						layoutMode:'masonry',
+						resizesContainer:true,
+						animationEngine:'jQuery',
+						filter: '*'
+					});
+				    window.lightbox.option({
+				      'resizeDuration': 200,
+				      'wrapAround': true
+				    });
+					clearInterval(window.isotopeTimer);
+				}, 50);
+			}
+		  }
+		}
 	});
--- scripts/db.js
+++ scripts/db.js
@@ -1,18 +1,102 @@
 var laszlovectorElements =
 	[
 		{
-			thumbnailImg : 'http://laszlovector.bogi-solutions.com/images/uploaded/1_me_2.jpg',
-			description: 'Portfolio',
-			cssClass: 'isotope_w1_h2',
+			thumbnailImg : "../images/ref_280x160_beugros.jpg",
+			description: 'Beugros',
+			cssClass: 'isotope_w1_h1',
 			link : '',
-			images: []
+			images: ["../images/Beugro_Logo _a_MockUp_1.jpg", "../images/Beugro_Logo_b_v3_MockUp_1.jpg"]
 		},
 		{
-			thumbnailImg : 'http://laszlovector.bogi-solutions.com/images/uploaded/2_cv_hu.jpg',
-			description: 'CV hu',
+			thumbnailImg : "../images/ref_280x330_consider_invest.jpg",
+			description: 'Consider_Invest',
+			cssClass: 'isotope_w1_h2',
+			link : '',
+			images: ["../images/CI_nevjegy_mockup.jpg", "../images/consider_invest_logo.jpg"]
+		},
+		{
+			thumbnailImg : "../images/ref_280x160_europa.jpg",
+			description: 'Europa',
 			cssClass: 'isotope_w1_h1',
-			link : 'http://laszlovector.bogi-solutions.com/images/uploaded/cv/cv_fl_hun.pdf',
+			link : '',
+			images: ["../images/ref_280x160_europa.jpg", "../images/ke_press_127x44_pre.jpg"]
+		},
+		{
+			thumbnailImg : "../images/ref_280x160_interactive.jpg",
+			description: 'interactive',
+			cssClass: 'isotope_w1_h1',
+			link : '',
+			images: ["../images/bx_a__logo.jpg"]
+		},
+		{
+			thumbnailImg : "../images/ref_280x330_iport.jpg",
+			description: 'iport',
+			cssClass: 'isotope_w1_h2',
+			link : '',
+			images: ["../images/1_mappa_pre.jpg", "../images/fd1.jpg"]
+		},
+		{
+			thumbnailImg : "../images/ref_280x160_logo.jpg",
+			description: 'logo',
+			cssClass: 'isotope_w1_h1',
+			link : '',
+			images: ["../images/ref_280x160_logo.jpg", "../images/gazdagreti_logok.jpg"]
+		},
+		{
+			thumbnailImg : "../images/ref_280x160_nyugtalotto.jpg",
+			description: 'nyugtalotto',
+			cssClass: 'isotope_w1_h1',
+			link : '',
+			images: ["../images/nyugtalotto_b_MockUp.jpg", "../images/nyugtalotto_boritek_LA4_mockup.jpg"]
+		},
+		{
+			thumbnailImg : "../images/ref_280x160_online_advert.jpg",
+			description: 'online_advert',
+			cssClass: 'isotope_w1_h1',
+			link : '',
+			images: ["../images/Femcafe_skin_pre.jpg", "../images/hiper_web_menufejleszt.jpg"]
+		},
+		{
+			thumbnailImg : "../images/ref_280x160_oszk.jpg",
+			description: 'oszk',
+			cssClass: 'isotope_w1_h1',
+			link : '',
+			images: ["../images/2_mappa_pre.jpg", "../images/3_mappa_pre.jpg"]
+		},
+		{
+			thumbnailImg : "../images/ref_280x160_port.jpg",
+			description: 'port',
+			cssClass: 'isotope_w1_h1',
+			link : '',
+			images: ["../images/bobitanet_kerdezz_szakertooldal.jpg", "../images/bobitanet_kerdezz_szakertooldal_wireframe.jpg"]
+		},
+		{
+			thumbnailImg : "../images/ref_280x160_press_stuffs.jpg",
+			description: 'press_stuffs',
+			cssClass: 'isotope_w1_h1',
+			link : '',
+			images: ["../images/flyer_mockup_1.jpg", "../images/flyer_mockup_2.jpg"]
+		},
+		{
+			thumbnailImg : "../images/ref_280x160_prima.jpg",
+			description: 'prima',
+			cssClass: 'isotope_w1_h1',
+			link : '',
+			images: ["../images/primakupon_mockup_1.jpg", "../images/primakupon_mockup_2.jpg"]
+		},
+		{
+			thumbnailImg : "../images/ref_280x160_sport.jpg",
+			description: 'sport',
+			cssClass: 'isotope_w1_h1',
+			link : '',
+			images: ["../images/IIKER_mockup_1_a.jpg", "../images/IIKER_mockup_1_b.jpg"]
+		},
+		{
+			thumbnailImg : "../images/ref_280x160_banners.jpg",
+			description: 'banners',
+			cssClass: 'isotope_w1_h1',
+			link : '../images/banners/FL_banners.zip',
 			images: []
-		}
+		},
 	]
 ;
