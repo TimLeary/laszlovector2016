effd680fc6c3f80303f31026adcf634ecdf9b15f
TimLeary <dnasztanovics@gmail.com>
2016-03-02 19:08:58 +0100

    upgrade from working resource

--- scripts/db.js
+++ scripts/db.js
@@ -1,47 +1,56 @@
 var laszlovectorElements =
 	[
 		{
-		    thumbnailImg: "../images/ref_280x160_beugros.jpg",
-		    title: 'Beugros',
+		    thumbnailImg: "../images/ref_280x160_port.jpg",
+		    title: 'port',
 		    cssClass: 'isotope_w1_h1',
 		    link: '',
-		    images: ["../images/Beugro_Logo _a_MockUp_1.jpg", "../images/Beugro_Logo_b_v3_MockUp_1.jpg"],
-		    showDescription: true,
-		    description: '<h4 class="klassz">Leiras a cuccokrol hosszabban meg blabla</h4>'
+		    images: ["../images/port_01.jpg", "../images/port_02.jpg", "../images/port_03.jpg", "../images/port_04.jpg", "../images/port_05.jpg" ],
+		    showDescription: false,
+		    description: ''
 		},
 		{
-		    thumbnailImg: "../images/ref_280x330_consider_invest.jpg",
-		    title: 'Consider_Invest',
+		    thumbnailImg: "../images/ref_280x330_beugros.jpg",
+		    title: 'Beugros',
 		    cssClass: 'isotope_w1_h2',
 		    link: '',
-		    images: ["../images/CI_nevjegy_mockup.jpg", "../images/consider_invest_logo.jpg"],
+		    images: ["../images/beugros_01.jpg", "../images/beugros_02.jpg","../images/beugros_03.jpg","../images/beugros_04.jpg","../images/beugros_05.jpg","../images/beugros_06.jpg","../images/beugros_07.jpg","../images/beugros_01.jpg"],
 		    showDescription: true,
-		    description: 'Lorem ipsum dolor'
-		},
-		{
-		    thumbnailImg: "../images/ref_280x160_europa.jpg",
-		    title: 'Europa',
-		    cssClass: 'isotope_w1_h1',
-		    link: '',
-		    images: ["../images/ke_press_127x44_pre.jpg"],
-		    showDescription: true,
-		    description: '<h4 class="klassz">Leiras a cuccokrol hosszabban meg blabla es meg tobb blabla</h4>'
+		    description: '<h4 class="klassz">Leiras a cuccokrol hosszabban meg blabla</h4>'
 		},
 		{
 		    thumbnailImg: "../images/ref_280x160_interactive.jpg",
 		    title: 'interactive',
 		    cssClass: 'isotope_w1_h1',
 		    link: '',
-		    images: ["../images/bx_a__logo.jpg"],
+		    images: ["../images/interactive_01.jpg", "../images/interactive_02.jpg", "../images/interactive_03.jpg", "../images/interactive_04.jpg", "../images/interactive_05.jpg", "../images/interactive_06.jpg", "../images/interactive_07.jpg", "../images/interactive_08.jpg", "../images/interactive_09.jpg", "../images/interactive_10.jpg", "../images/interactive_11.jpg", "../images/interactive_12.jpg", "../images/interactive_13.jpg" ],
 		    showDescription: false,
 		    description: '<h4 class="klassz">blabla es megint blabla</h4>'
+		},
+		{
+		    thumbnailImg: "../images/ref_280x330_consider_invest.jpg",
+		    title: 'Consider_Invest',
+		    cssClass: 'isotope_w1_h2',
+		    link: '',
+		    images: ["../images/consider_01.jpg", "../images/consider_02.jpg", "../images/consider_03.jpg", "../images/consider_04.jpg", "../images/consider_05.jpg", "../images/consider_06.jpg", "../images/consider_07.jpg", "../images/consider_08.jpg"],
+		    showDescription: true,
+		    description: 'Lorem ipsum dolor'
+		},
+		{
+		    thumbnailImg: "../images/ref_280x330_europa.jpg",
+		    title: 'Europa',
+		    cssClass: 'isotope_w1_h2',
+		    link: '',
+		    images: ["../images/europa_01.jpg", "../images/europa_02.jpg", "../images/europa_03.jpg" ],
+		    showDescription: true,
+		    description: '<h4 class="klassz">Leiras a cuccokrol hosszabban meg blabla es meg tobb blabla</h4>'
 		},
 		{
 		    thumbnailImg: "../images/ref_280x330_iport.jpg",
 		    title: 'iport',
 		    cssClass: 'isotope_w1_h2',
 		    link: '',
-		    images: ["../images/1_mappa_pre.jpg", "../images/fd1.jpg"],
+		    images: ["../images/iport_01.jpg", "../images/iport_02.jpg", "../images/iport_03.jpg", "../images/iport_04.jpg", "../images/iport_05.jpg", "../images/iport_06.jpg", "../images/iport_07.jpg", "../images/iport_08.jpg", "../images/iport_09.jpg", "../images/iport_10.jpg" ],
 		    showDescription: false,
 		    description: ''
 		},
@@ -50,7 +59,7 @@ var laszlovectorElements =
 		    title: 'logo',
 		    cssClass: 'isotope_w1_h1',
 		    link: '',
-		    images: ["../images/gazdagreti_logok.jpg"],
+		    images: [ "../images/logo_01.jpg", "../images/logo_02.jpg", "../images/logo_03.jpg", "../images/logo_04.jpg", "../images/logo_05.jpg", "../images/logo_06.jpg", "../images/logo_07.jpg", "../images/logo_08.jpg"],
 		    showDescription: false,
 		    description: ''
 		},
@@ -59,7 +68,7 @@ var laszlovectorElements =
 		    title: 'nyugtalotto',
 		    cssClass: 'isotope_w1_h1',
 		    link: '',
-		    images: ["../images/nyugtalotto_b_MockUp.jpg", "../images/nyugtalotto_boritek_LA4_mockup.jpg"],
+		    images: ["../images/nyugtalotto_01.jpg", "../images/nyugtalotto_02.jpg", "../images/nyugtalotto_03.jpg", "../images/nyugtalotto_04.jpg" ],
 		    showDescription: false,
 		    description: ''
 		},
@@ -68,52 +77,43 @@ var laszlovectorElements =
 		    title: 'online_advert',
 		    cssClass: 'isotope_w1_h1',
 		    link: '',
-		    images: ["../images/Femcafe_skin_pre.jpg", "../images/hiper_web_menufejleszt.jpg"],
+		    images: ["../images/online_advert_01.jpg", "../images/online_advert_02.jpg", "../images/online_advert_03.jpg" ],
 		    showDescription: false,
 		    description: ''
 		},
 		{
-		    thumbnailImg: "../images/ref_280x160_oszk.jpg",
+		    thumbnailImg: "../images/ref_280x330_oszk.jpg",
 		    title: 'oszk',
-		    cssClass: 'isotope_w1_h1',
+		    cssClass: 'isotope_w1_h2',
 		    link: '',
-		    images: ["../images/2_mappa_pre.jpg", "../images/3_mappa_pre.jpg"],
+		    images: ["../images/oszk_01.jpg", "../images/oszk_02.jpg", "../images/oszk_03.jpg", "../images/oszk_04.jpg", "../images/oszk_05.jpg"],
 		    showDescription: false,
 		    description: ''
 		},
 		{
-		    thumbnailImg: "../images/ref_280x160_port.jpg",
-		    title: 'port',
-		    cssClass: 'isotope_w1_h1',
+		    thumbnailImg: "../images/ref_280x330_print_stuffs.jpg",
+		    title: 'print_stuffs',
+		    cssClass: 'isotope_w1_h2',
 		    link: '',
-		    images: ["../images/bobitanet_kerdezz_szakertooldal.jpg", "../images/bobitanet_kerdezz_szakertooldal_wireframe.jpg"],
-		    showDescription: false,
-		    description: ''
-		},
-		{
-		    thumbnailImg: "../images/ref_280x160_press_stuffs.jpg",
-		    title: 'press_stuffs',
-		    cssClass: 'isotope_w1_h1',
-		    link: '',
-		    images: ["../images/flyer_mockup_1.jpg", "../images/flyer_mockup_2.jpg"],
-		    showDescription: false,
-		    description: ''
+		    images: ["../images/print_stuffs_01.jpg", "../images/print_stuffs_02.jpg", "../images/print_stuffs_03.jpg", "../images/print_stuffs_04.jpg", "../images/print_stuffs_05.jpg", "../images/print_stuffs_06.jpg", "../images/print_stuffs_07.jpg", "../images/print_stuffs_08.jpg", "../images/print_stuffs_09.jpg", "../images/print_stuffs_10.jpg", "../images/print_stuffs_11.jpg", "../images/print_stuffs_12.jpg", "../images/print_stuffs_13.jpg", "../images/print_stuffs_14.jpg", "../images/print_stuffs_15.jpg", "../images/print_stuffs_16.jpg", "../images/print_stuffs_17.jpg", "../images/print_stuffs_18.jpg", "../images/print_stuffs_19.jpg", "../images/print_stuffs_20.jpg", "../images/print_stuffs_21.jpg" ],
+		    showDescription: true,
+		    description: '<h4 class="klassz">Leiras a cuccokrol hosszabban meg blabla es meg tobb blabla</h4>'
 		},
 		{
 		    thumbnailImg: "../images/ref_280x160_prima.jpg",
 		    title: 'prima',
 		    cssClass: 'isotope_w1_h1',
 		    link: '',
-		    images: ["../images/primakupon_mockup_1.jpg", "../images/primakupon_mockup_2.jpg"],
+		    images: ["../images/prima_01.jpg", "../images/prima_02.jpg", "../images/prima_03.jpg", "../images/prima_04.jpg", "../images/prima_05.jpg", "../images/prima_06.jpg", "../images/prima_07.jpg", "../images/prima_08.jpg", "../images/prima_09.jpg"],
 		    showDescription: false,
 		    description: ''
 		},
 		{
-		    thumbnailImg: "../images/ref_280x160_sport.jpg",
-		    title: 'sport',
+		    thumbnailImg: "../images/ref_280x160_florida.jpg",
+		    title: 'florida',
 		    cssClass: 'isotope_w1_h1',
 		    link: '',
-		    images: ["../images/IIKER_mockup_1_a.jpg", "../images/IIKER_mockup_1_b.jpg"],
+		    images: ["../images/florida_01.jpg", "../images/florida_02.jpg", "../images/florida_03.jpg", "../images/florida_04.jpg", "../images/florida_05.jpg", "../images/florida_06.jpg", "../images/florida_07.jpg", ],
 		    showDescription: false,
 		    description: ''
 		},
